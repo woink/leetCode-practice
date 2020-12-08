41d68e8ad97bc885e1d8f31db55c7458cb51b543
woink <wardprice@gmail.com>
2020-12-08 01:27:07 -0500

    first two bonus challenges completed

--- recursionBonusProblemSet.js
+++ recursionBonusProblemSet.js
@@ -1,1 +1,16 @@
+function reverse(str) {
+  if (str.length <= 1) return str
+  console.log(str)
+  return reverse(str.slice(1)) + str[0]
+}
 
+// console.log(reverse("hello world"))
+
+function isPalindrome(str) {
+  if (str.length === 1) return true
+  if (str.length === 2) return str[0] === str[1]
+  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1))
+  return false
+}
+
+console.log(isPalindrome('awesome'))
