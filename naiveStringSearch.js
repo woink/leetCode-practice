8620fc8417c6c50967254fbca6c79828f92241c3
woink <wardprice@gmail.com>
2020-12-08 00:21:08 -0500

    set filenames to camel case

--- naiveStringSearch.js
+++ naiveStringSearch.js
@@ -9,7 +9,8 @@ function naiveSearch(long, short) {
   let count = 0
   for (let i = 0; i < long.length; i++) {
     for (let j = 0; j < short.length; j++) {
-      // long[i + j] ens ures iteration will go through and reset the short word
+      // long[i + j] ens 
+      ures iteration will go through and reset the short word
       if (short[j] !== long[i + j]) break
       if (j = short.length -1) count++
     }
