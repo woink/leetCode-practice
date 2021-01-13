09b5103a3231cb157206ee1e51fd441b7e2c096e
woink <wardprice@gmail.com>
2021-01-13 14:09:25 -0500

    subtractProductAndSum.ts

--- subtractProductAndSum.ts
+++ subtractProductAndSum.ts
@@ -1,4 +1,13 @@
 function subtractProductAndSum(n: number): number {
-  let sum = n.reduce((a, b) => a + b)
+  let nProduct = 1,
+    nSum = 0
   
+  for (const num of n.toString()) {
+    nProduct *= +num
+    nSum += +num
+  }
+  return nProduct - nSum
 };
+
+let n = 234
+console.log(subtractProductAndSum(n))
