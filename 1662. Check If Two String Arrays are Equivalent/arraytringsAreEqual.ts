02c6f0a532198e5cad9ce365f82478d88a21e4a9
woink <wardprice@gmail.com>
2021-01-23 09:58:30 -0500

    feat(solved): :art: more concise solution

--- 1662. Check If Two String Arrays are Equivalent/arraytringsAreEqual.ts
+++ 1662. Check If Two String Arrays are Equivalent/arraytringsAreEqual.ts
@@ -3,10 +3,15 @@ function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
 		word2Join = word2.join('');
 
 	if (word1Join.length !== word2Join.length) return false;
+
 	for (let i = 0; i < word1Join.length; i++) {
 		if (word1Join[i] !== word2Join[i]) return false;
 	}
 	return true;
+}
+
+function arrayStringsAreEqual2(word1: string[], word2: string[]): boolean {
+	return word1.join('') === word2.join('');
 }
 
 // const word1 = ['ab', 'c'],
@@ -16,3 +21,4 @@ function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
 const word1 = ['abc', 'd', 'defg'],
 	word2 = ['abcddefg'];
 console.log(arrayStringsAreEqual(word1, word2));
+console.log(arrayStringsAreEqual2(word1, word2));
