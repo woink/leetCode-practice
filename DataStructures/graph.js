b5f705dbb2e942620e375ef0176ccfdb359291b3
woink <wardprice@gmail.com>
2021-02-11 22:56:12 -0500

    feat(DataStructure): :sparkles: add examples how to represent graphs

---
+++ DataStructures/graph.js
@@ -0,0 +1,18 @@
+//Edge List
+const edgeListGraph = [
+	[0, 2],
+	[2, 3],
+	[2, 1],
+	[1, 3],
+];
+
+//Adjacent List
+const adjacentListGraph = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+//Adjacent Matrix
+const adjacentMatrixGraph = {
+	0: [0, 0, 1, 0],
+	1: [0, 0, 1, 1],
+	2: [1, 1, 0, 1],
+	3: [0, 1, 1, 0],
+};
