2d2e277cac46f2259391d7ce2fc6ba95728c9123
woink <wardprice@gmail.com>
2021-02-12 16:02:29 -0500

    feat(algorithms): :sparkles: quick sort

--- Algorithms/sorting.ts
+++ Algorithms/sorting.ts
@@ -47,7 +47,7 @@ function insertionSort(array: number[]) {
 	return array;
 }
 
-// Divide and Conqure
+// Divide and Conqure O(n log n)
 // Merge Sort
 
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
@@ -79,4 +79,44 @@ function merge(left: number[], right: number[]) {
 const answer = mergeSort(numbers);
 console.log(answer)
 
+// Divide and Conqure O(n log n)
+// Quick Sort
 
+function quickSort(array: number[], left:number, right:number){
+  let pivot;
+  let partitionIndex;
+
+  if(left < right) {
+    pivot = right;
+    partitionIndex = partition(array, pivot, left, right);
+    
+    //sort left and right
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, right);
+  }
+  return array;
+}
+   
+function partition(array:number[], pivot: number, left: number, right: number){
+  const pivotValue = array[pivot];
+  let partitionIndex = left;
+
+  for(let i = left; i < right; i++) {
+    if(array[i] < pivotValue){
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(array, right, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(array: number[], firstIndex: number, secondIndex: number){
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+}
+
+//Select first and last index as 2nd and 3rd parameters
+quickSort(numbers, 0, numbers.length - 1);
+console.log(numbers);
