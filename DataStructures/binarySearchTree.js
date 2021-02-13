2945a0a995e5336d2bec0ec3e43e8368efdb8617
woink <wardprice@gmail.com>
2021-02-12 19:00:27 -0500

    feat(algorithms): :sparkles: breadth first search

--- DataStructures/binarySearchTree.js
+++ DataStructures/binarySearchTree.js
@@ -11,44 +11,63 @@ class BinarySearchTree {
 		this.root = null;
 	}
 
-  insert(value) {
-    const newNode = new Node(value)
-    if (!this.root) {
-      this.root = newNode
-    } else {
-      let currentNode = this.root
-      while (true) {
-        if (value < currentNode.value) {
-          if (!currentNode.left) {
-            currentNode.left = newNode
-            return this
-          }
-          currentNode = currentNode.left
-        } else {
-          if (!currentNode.right) {
-            currentNode.right = newNode
-            return this
-          }
-          currentNode = currentNode.right
-        }
-      }
-    }
-  }
+	insert(value) {
+		const newNode = new Node(value);
+		if (!this.root) {
+			this.root = newNode;
+		} else {
+			let currentNode = this.root;
+			while (true) {
+				if (value < currentNode.value) {
+					if (!currentNode.left) {
+						currentNode.left = newNode;
+						return this;
+					}
+					currentNode = currentNode.left;
+				} else {
+					if (!currentNode.right) {
+						currentNode.right = newNode;
+						return this;
+					}
+					currentNode = currentNode.right;
+				}
+			}
+		}
+	}
 
-  lookup(value) {
-    if (!this.root) return false
-    let currentNode = this.root
-    while (currentNode) {
-      if (value < currentNode.value) {
-        currentNode = currentNode.left
-      } else if (value > currentNode.value) {
-        currentNode = currentNode.right
-      } else if (currentNode.value === value) {
-        return currentNode
-      } 
+	lookup(value) {
+		if (!this.root) return false;
+		let currentNode = this.root;
+		while (currentNode) {
+			if (value < currentNode.value) {
+				currentNode = currentNode.left;
+			} else if (value > currentNode.value) {
+				currentNode = currentNode.right;
+			} else if (currentNode.value === value) {
+				return currentNode;
+			}
+		}
+		return false;
+	}
+
+	breadthFirstSearch() {
+		let currentNode = this.root;
+		const list = [],
+			queue = [];
+		queue.push(currentNode);
+
+    while (queue.length > 0) {
+			currentNode = queue.shift();
+			list.push(currentNode.value);
+			if (currentNode.left) {
+				queue.push(currentNode.left);
+			}
+			if (currentNode.right) {
+				queue.push(currentNode.right);
+			}
     }
-    return false
-  }
+    return list
+	}
 }
 
 const tree = new BinarySearchTree();
@@ -59,12 +78,12 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
-tree.lookup(9)
-console.log(tree.lookup(9))
+tree.lookup(9);
+console.log(tree.lookup(9));
 
 function traverse(node) {
-  const tree = { value: node.value };
-  tree.left = node.left === null ? null : traverse(node.left);
-  tree.right = node.right === null ? null : traverse(node.right);
-  return tree;
+	const tree = { value: node.value };
+	tree.left = node.left === null ? null : traverse(node.left);
+	tree.right = node.right === null ? null : traverse(node.right);
+	return tree;
 }
